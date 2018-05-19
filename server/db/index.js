da2e9a534c7891b61058757698704912da89dca2
reard96 <reardon.danielj@gmail.com>
2018-05-19 17:00:28 -0400

    more server organization

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,22 @@
 const conn = require('./conn');
 const Dashboard = require('./models/Dashboard');
 
+const sync = () => {
+  return conn.sync({ force: true });
+};
+
+const seed = () => {
+  return Promise.all([
+    Dashboard.create({ goal: 'exercise' }),
+    Dashboard.create({ goal: 'practice reduce '}),
+    Dashboard.create({ goal: 'foo, bar, baz'})
+  ]);
+};
+
 module.exports = {
   conn,
+  sync,
+  seed,
   models: {
     Dashboard
   }
---
+++ server/server.js
@@ -0,0 +1,36 @@
+ const express = require('express');
+const app = express();
+const path = require('path');
+const db = require('./db');
+
+app.use(require('body-parser').json());
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+// app.use PUBLIC
+
+app.use('/api', require('./api/dashboards'));
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+db.sync()
+  .then(() => db.seed());
+
+module.exports = app;
+
+// const conn = require('./db/conn');
+
+// const Dashboard = require('./db/models/Dashboard');
+
+// conn.sync({ force: true })
+//   .then(() => {
+//     return Promise.all([
+//       Dashboard.create({ goal: 'exercise' }),
+//       Dashboard.create({ goal: 'practice reduce '}),
+//       Dashboard.create({ goal: 'foo, bar, baz'})
+//     ]);
+//   });
+
+
