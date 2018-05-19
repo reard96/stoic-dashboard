b4f6ac1956c6cebb5deab18370aeba84fd2cc188
reard96 <reardon.danielj@gmail.com>
2018-05-19 14:01:10 -0400

    set up react

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.use(require('body-parser').json());
 
