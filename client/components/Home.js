1c8eb04fafc22bb92cb5cbbbba41cd66a1ba3bef
reard96 <reardon.danielj@gmail.com>
2018-05-19 18:15:40 -0400

    styling

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { PageHeader } from 'react-bootstrap';
+import style from '../../public/style.css';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div className={ style.wrapper }>
+      <PageHeader>Hello, Dan</PageHeader>
+    </div>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,16 @@ module.exports = {
         query: {
           presets: [ 'es2015', 'react']
         }
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
       }
     ]
   }
