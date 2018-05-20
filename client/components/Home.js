a5fa43c081c9d8166cc7ff3980b0696f1d60cd38
reard96 <reardon.danielj@gmail.com>
2018-05-20 15:31:46 -0400

    removed some hardcoding

--- client/components/Home.js
+++ client/components/Home.js
@@ -12,16 +12,6 @@ import { connect } from 'react-redux';
 import { PageHeader, Label, ProgressBar } from 'react-bootstrap';
 
 const Home = ({ user }) => {
-  // hardcoding for now - Moment objects
-  const birthday = moment('1993-05-09');
-  const now = moment();
-
-  // Date strings
-  const dob = birthday.format('LL');
-  const today = now.format('LL');
-  const diff = now.diff(birthday, 'days');
-  // Ignore leap years
-  const timeLeft = 90 * 365 - diff;
   if (!user) {
     return (
       <div className="wrapper">
@@ -29,21 +19,33 @@ const Home = ({ user }) => {
       </div>
     );
   }
-  return (
-    <div className="wrapper">
-      <PageHeader>Hello, { user.name }</PageHeader>
-      <h4>You were born on: { dob }</h4>
-      <h4>Today is: { today }</h4>
-      <h4>You have been alive for <Label bsStyle="warning">{ diff }</Label> days</h4>
-      <h4>Assuming you'll live to 90, you have <Label bsStyle="info">{ timeLeft }</Label> days left</h4>
-      <h4>Here's how much time you have on your annual goals:</h4>
-      <div>
-        <ProgressBar active className="progressBar" now={ 38 } label="38%" bsStyle="warning" />
+  else {
+    const birthday = moment(user.birthday);
+    //const birthday = moment('1993-05-09');
+    const now = moment();
+
+    // Date strings
+    const dob = birthday.format('LL');
+    const today = now.format('LL');
+    const diff = now.diff(birthday, 'days');
+    // Ignore leap years
+    const timeLeft = 90 * 365 - diff;
+    return (
+      <div className="wrapper">
+        <PageHeader>Hello, { user.name }</PageHeader>
+        <h4>You were born on: { dob }</h4>
+        <h4>Today is: { today }</h4>
+        <h4>You have been alive for <Label bsStyle="warning">{ diff }</Label> days</h4>
+        <h4>Assuming you'll live to 90, you have <Label bsStyle="info">{ timeLeft }</Label> days left</h4>
+        <h4>Here's how much time you have on your annual goals:</h4>
+        <div>
+          <ProgressBar active className="progressBar" now={ 38 } label="38%" bsStyle="warning" />
+        </div>
+        <h4>Twitter agrees:</h4>
+        <TweetEmbed id="997884591653433344" />
       </div>
-      <h4>Twitter agrees:</h4>
-      <TweetEmbed id="997884591653433344" />
-    </div>
-  );
+    );
+  }
 };
 
 const mapStateToProps = ({ users }) => {
